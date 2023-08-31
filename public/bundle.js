552d968f36cb5e5deefe8f3d36dcdf491832b313
henry paredes <you@example.com>
2023-08-30 21:09:20 -0400

    new js files

--- public/bundle.js
+++ public/bundle.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var data = {
+var dataFotos = {
 	fotos: {
 		america: [
 			{
@@ -431,7 +431,7 @@ var data = {
 	},
 };
 
-const { fotos } = data;
+const { fotos } = dataFotos;
 
 var dataCategorias = {
 	categorias: [
@@ -470,14 +470,41 @@ categorias.forEach((categoria) => {
 });
 
 const contenedorCategorias = document.getElementById('categorias');
-const galeria = document.getElementById('galeria');
+const galeria$2 = document.getElementById('galeria');
 
 contenedorCategorias.addEventListener('click', (e) => {
   e.preventDefault();
-
   if (e.target.closest('a')) {
-    galeria.classList.add('galeria--active');
+    galeria$2.classList.add('galeria--active');
     document.body.style.overflow = 'hidden';
+    
+    const categoriaActiva = e.target.dataset.categoria;
+    const fotos = dataFotos.fotos[categoriaActiva];
+    fotos.forEach((foto) => {
+      const slide = `
+        <a href="#" class="galeria__carousel-slide">
+          <img class="galeria__carousel-image" src="${foto.ruta}" alt="" />
+        </a>
+      `;
+      galeria$2.querySelector('.galeria__carousel-slides').innerHTML += slide;
+    });
 
+    galeria$2.querySelector('.galeria__carousel-slide').classList.add('galeria__carousel-slide--active');
+  }
+});
+
+const galeria$1 = document.getElementById('galeria');
+const cerrarGaleria = () => {
+  galeria$1.classList.remove('galeria--active');
+
+  document.body.style.overflow = '';
+};
+
+const galeria = document.getElementById('galeria');
+galeria.addEventListener('click', (e) => {
+  const boton = e.target.closest('button');
+
+  if(boton?.dataset?.accion === 'cerrar-galeria') {
+    cerrarGaleria();
   }
 });
--- src/eventoCategorias.js
+++ src/eventoCategorias.js
@@ -1,12 +1,24 @@
+import dataFotos from './datos/fotos';
 const contenedorCategorias = document.getElementById('categorias');
 const galeria = document.getElementById('galeria');
 
 contenedorCategorias.addEventListener('click', (e) => {
   e.preventDefault();
-
   if (e.target.closest('a')) {
     galeria.classList.add('galeria--active');
     document.body.style.overflow = 'hidden';
+    
+    const categoriaActiva = e.target.dataset.categoria;
+    const fotos = dataFotos.fotos[categoriaActiva];
+    fotos.forEach((foto) => {
+      const slide = `
+        <a href="#" class="galeria__carousel-slide">
+          <img class="galeria__carousel-image" src="${foto.ruta}" alt="" />
+        </a>
+      `;
+      galeria.querySelector('.galeria__carousel-slides').innerHTML += slide;
+    });
 
+    galeria.querySelector('.galeria__carousel-slide').classList.add('galeria__carousel-slide--active')
   }
 });
